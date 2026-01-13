934fcb072b1c241b547301087f4d6326f8e06560
tabreturn <github@tabreturn.com>
2026-01-14 10:12:39 +1100

    - update sh script

--- ports/escapefromcastlematsumoto/Escape from Castle Matsumoto.sh
+++ ports/escapefromcastlematsumoto/Escape from Castle Matsumoto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# portmaster preamble
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
-
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
@@ -12,15 +12,14 @@ else
   controlfolder="/roms/ports/PortMaster"
 fi
 
-source $controlfolder/control.txt
-
-[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-
-get_controls
-
+# script variables
 GAMEDIR="/$directory/ports/escapefromcastlematsumoto"
 BINARY=efcm
 
+# controls and logging
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 # setup conf directory
@@ -28,15 +27,15 @@ CONFDIR="$GAMEDIR/conf"
 $ESUDO mkdir -p "$CONFDIR"
 bind_directories "$HOME/.local/share/Escape From Castle Matsumoto" "$CONFDIR"
 
-cd $GAMEDIR
-
-mv lib*.so* ./libs.aarch64
-mv *.txt ./licenses
-
+# exports
 export LD_LIBRARY_PATH="$GAMEDIR/libs.aarch64:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
+# run game
+cd $GAMEDIR
 $GPTOKEYB "$BINARY" -c ./$BINARY.gptk &
 pm_platform_helper "$GAMEDIR/$BINARY"
-./$BINARY 
+./$BINARY
+
+# clean up
 pm_finish
