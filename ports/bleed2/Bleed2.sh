4b11c4a195d4c9589c066475f1680a84dadb8ded
ImCoKeMaN <25371653+ImCoKeMaN@users.noreply.github.com>
2026-03-19 17:38:24 -0400

    Refactor savedir setup in Bleed2.sh
    Replaced manual directory setup with bind_directories function.

--- ports/bleed2/Bleed2.sh
+++ ports/bleed2/Bleed2.sh
@@ -35,9 +35,7 @@ $ESUDO umount "$monofile" || true
 $ESUDO mount "$monofile" "$monodir"
 
 # Setup savedir
-$ESUDO rm -rf ~/.local/share/Bleed2
-mkdir -p ~/.local/share
-ln -sfv "$gamedir/savedata" ~/.local/share/Bleed2
+bind_directories ~/.local/share/Bleed2 "$gamedir/savedata"
 
 # Remove all the dependencies in favour of system libs - e.g. the included 
 # newer version of FNA with patcher included
