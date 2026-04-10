92dff6e7c0468a82386d2d1e5e475e4485486a96
tabreturn <github@tabreturn.com>
2026-04-11 08:58:34 +1000

    - add wizard's lair

---
+++ ports/blitzmax/The Wizard's Lair.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# portmaster preamble
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+GAMEDIR=/$directory/ports/thewizardslair
+game_executable="twl"
+#game_libs=$GAMEDIR/libs.${DEVICE_ARCH}/:$LD_LIBRARY_PATH
+x11sdl_path="$GAMEDIR/x11sdllib/"
+
+# logging
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# mount Weston runtime
+weston_dir=/tmp/weston
+$ESUDO mkdir -p "${weston_dir}"
+weston_runtime="weston_pkg_0.2"
+if [ ! -f "$controlfolder/libs/${weston_runtime}.squashfs" ]; then
+  if [ ! -f "$controlfolder/harbourmaster" ]; then
+    pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
+    sleep 5
+    exit 1
+  fi
+  $ESUDO $controlfolder/harbourmaster --quiet --no-check runtime_check "${weston_runtime}.squashfs"
+fi
+if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+  $ESUDO umount "${weston_dir}"
+fi
+$ESUDO mount "$controlfolder/libs/${weston_runtime}.squashfs" "${weston_dir}"
+
+cd $GAMEDIR
+
+$GPTOKEYB2 "$game_executable" -c "$GAMEDIR/${game_executable}.ini" &
+# start westonpack
+$ESUDO env WRAPPED_LIBRARY_PATH_MALI="$x11sdl_path" WRAPPED_PRELOAD_MALI="$x11sdl_path/libSDL2-2.0.so.0" \
+SDL_VIDEO_X11_FORCE_EGL=1 WRAPPED_LIBRARY_PATH=$game_libs \
+$weston_dir/westonwrap.sh headless noop kiosk crusty_x11egl \
+WAYLAND_DISPLAY= $GAMEDIR/$game_executable
+
+# clean up
+$ESUDO $weston_dir/westonwrap.sh cleanup
+if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+  $ESUDO umount "${weston_dir}"
+fi
+pm_finish
+
---
+++ ports/blitzmax/thewizardslair/readme.txt
@@ -0,0 +1,46 @@
+// THE WIZARD'S LAIR v5yo //
+
+Please make any bug reports by email to lord.devlin+support@gmail.com or tweet to @magic_bane !
+
+Notes:
+ - DO NOT EXTRACT OVER AN OLD INSTALL OF THE WIZARD'S LAIR -
+Don't forget to check out the Changelog for a basic summary of the changes and updates since the last version!
+
+Bear in mind that old saves might break the game. If you experience issues with an old save game, start a new game!
+
+Compatibility and System Requirements:
+
+No sound ingame on Windows? Install OpenAL, included with the package. (oalinst.exe)
+Linux users should install package "libopenal1"
+
+For Optimal performance, please make sure you have all your drivers up to date and correctly configured!
+
+Minimum requirements:
+CPU - 1.60GHz Atom or better equivalent.
+RAM - 512MB minimum.
+Graphics card - Anything with at least 64MB of Video RAM and has OpenGL support.
+Storage - At least 5MB of free HDD space.
+
+OS:
+
+Windows Vista or newer - Windows XP is compatible but not supported.
+Ubuntu 12.10 or newer - x64 users will be required to install the relevant 32-bit packages (package ia32libs) and the OpenAL library (package libopenal1)
+
+Game Controls - Keyboard and Gamepad:
+
+Arrow Keys or Joystick X/Y Axis(Left Stick) - Movement
+SPACE or Controller Button 1(A Button) - Confirm / Action / Pass Turn
+SHIFT or Controller Button 2(B Button) - Cancel / Enter Menu
+F12 - Take Screenshot (they are saved in the same folder as the game executable)
+ESC - Fast Quit (save and exit direct to OS, will ask to confirm)
+
+Game Credits:
+Game Code(with assistance from Madgarden), Sound, Concept and Story by Devlin
+Graphics by Devlin and Madgarden(thanks dude)
+Lore Book written by Erin Wright
+
+Special Thanks:
+Members of The Sanctuary(you know who you are!)
+Madgarden
+My followers @ Twitter
+My sister, Erin for her feedback!
