6a4681d41f1c68adfbad77b3e3bb8f433aad30ca
tabreturn <github@tabreturn.com>
2026-05-04 18:19:42 +1000

    - add metadata and launch sh

---
+++ ports/whimtale/README.md
@@ -0,0 +1,24 @@
+# Whimtale PM
+
+A PortMaster player for [Whimtale](https://whimtale.com) games -- a fantasy workstation for making tiny pixel-art adventure games. Based on [jsgamelauncher](https://github.com/monteslu/jsgamelauncher) by Luis Montes.
+
+Drop `.whim` or `.whimsy` game files into the `games/` folder and play.
+
+
+## Controls
+
+| Button        | Action             |
+|---------------|--------------------|
+| D-PAD/L-STICK | Move               |
+| A             | Interact / Dismiss |
+| L1            | Save               |
+| R1            | Load               |
+| SELECT+START  | Exit               |
+
+
+## Adding Games
+
+Put `.whim` or `.whimsy` files in `whimtalepm/games/`. That's it -- no subdirectories, no conversion, no config.
+
+If only one game file is present, it loads immediately. With multiple games, a menu lets you pick.
+
---
+++ ports/whimtale/Whimtale PM.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+# pm
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# variables
+GAMEDIR="/$directory/ports/whimtalepm"
+ROM="$GAMEDIR/runner.js"
+
+# bind save data to port conf folder
+bind_directories ~/.jsgamelauncher/whimtalepm $GAMEDIR/conf
+
+# cd & logging
+cd "$GAMEDIR"
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# exports
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# extract node binary on first run
+if [ ! -f "$GAMEDIR/node" ]; then
+  pm_message "Extracting node binary ..."
+  if "$controlfolder/7zzs.$DEVICE_ARCH" x "$GAMEDIR/node.7z" -o"$GAMEDIR" -y; then
+    chmod +x "$GAMEDIR/node"
+    rm -f "$GAMEDIR/node.7z"
+  else
+    pm_message "extraction failed!"
+    pm_finish
+    exit 1
+  fi
+fi
+# extract node_modules on first run
+if [ ! -d "$GAMEDIR/node_modules" ]; then
+  pm_message "Extracting node_modules ..."
+  if "$controlfolder/7zzs.$DEVICE_ARCH" x "$GAMEDIR/node_modules.7z" -o"$GAMEDIR" -y; then
+    rm -f "$GAMEDIR/node_modules.7z"
+  else
+    pm_message "extraction failed!"
+    pm_finish
+    exit 1
+  fi
+fi
+
+# run
+if [[ "$CFW_NAME" = "ROCKNIX" ]]; then
+  export XDG_RUNTIME_DIR=/var/run/0-runtime-dir
+  export WAYLAND_DISPLAY=wayland-1
+  export SDL_VIDEODRIVER=wayland
+  export SDL_APP_ID=jsgamelauncher
+  export JSG_NO_EGL=1
+  # override bundled sdl2 with system wayland-capable one
+  if [ -f /usr/lib/libSDL2-2.0.so.0 ]; then
+    export LD_PRELOAD=/usr/lib/libSDL2-2.0.so.0
+  fi
+  $GPTOKEYB "node" -c "$GAMEDIR/inputs.gptk" &
+  pm_platform_helper "jsgamelauncher"
+  ./node ./index.js -rom "$ROM" -fullscreen
+else
+  $GPTOKEYB "node" -c "$GAMEDIR/inputs.gptk" &
+  pm_platform_helper "node"
+  ./node ./index.js -rom "$ROM" -fullscreen
+fi
+
+# cleanup
+pm_finish
+
